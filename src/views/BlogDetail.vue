d899cdd2815c06914ed8d868ebc41ed67e4ab851
jq <1820856361@qq.com>
2022-06-05 21:57:15 +0800

    1

---
+++ src/views/BlogDetail.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "BlogDetail.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/BlogEdit.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "BlogEdit.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
