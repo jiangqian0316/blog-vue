516eaa43dcbeb7f386bb7a87f45f8afa90e15286
jq <1820856361@qq.com>
2022-06-05 21:39:50 +0800

    1

--- src/main.js
+++ src/main.js
@@ -2,5 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Element from 'element-plus'
+import "element-ui/lib/theme-chalk/index.css"
+import axios from 'axios'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App) // 将默认改写为这样
+app.provide('$axios', axios)
+
+
+createApp(App).use(store).use(router).use(Element).mount('#app')
